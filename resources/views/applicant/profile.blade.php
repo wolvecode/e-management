e9b090ea3d318f164ab32626f363234b4173b367
Biodun Habeeb Azeez <biodun@steer.global>
2024-02-03 08:51:35 +0100

    add e-learning link to sidebar

--- resources/views/applicant/profile.blade.php
+++ resources/views/applicant/profile.blade.php
@@ -56,6 +56,24 @@
             <span class="ml-2 fonts-semibold text-lg tracking-wide truncate">Course</span>
         </a>
     </li>
+    @if (auth()->user()->institution == 'unilag')
+        <li>
+            <a href="https://www.linkedin.com/in/biodun-azeez"
+                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-[#325AB3] text-white hover:text-[#34A853] border-l-4 border-transparent hover:border-indigo-500 pl-6 py-8"
+                target="_blank">
+                <svg class="ml-4 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path
+                            d="M4 19V6.2C4 5.0799 4 4.51984 4.21799 4.09202C4.40973 3.71569 4.71569 3.40973 5.09202 3.21799C5.51984 3 6.0799 3 7.2 3H16.8C17.9201 3 18.4802 3 18.908 3.21799C19.2843 3.40973 19.5903 3.71569 19.782 4.09202C20 4.51984 20 5.0799 20 6.2V17H6C4.89543 17 4 17.8954 4 19ZM4 19C4 20.1046 4.89543 21 6 21H20M9 7H15M9 11H15M19 17V21"
+                            stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </g>
+                </svg>
+                <span class="ml-2 fonts-semibold text-lg tracking-wide truncate">E-Learning</span>
+            </a>
+        </li>
+    @endif
 @endsection
 
 @section('side')
