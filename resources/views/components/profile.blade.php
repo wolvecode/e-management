187b024a707d8dd7d9e106b298a1559a45d8f848
Biodun Habeeb Azeez <biodun@steer.global>
2024-02-03 09:02:24 +0100

    fix bugs on profile

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -102,7 +102,13 @@
                                 class="after:content-['*'] after:ml-0.5 after:text-red-500  text-sm font-medium text-slate-700">
                                 Institution
                             </span>
-                            <select name="institution" id=""
+                            <input type="text" name="institution"
+                                class="h-12 mt-1 px-3 py-2 bg-white capitalize
+                            border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none
+                            focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm 
+                            focus:ring-1"
+                                disabled placeholder="Enter Institution" value="{{ $user->institution }}" />
+                            {{-- <select name="institution" id=""
                                 class="h-12 px-3 py-2 bg-white
                             border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none
                             focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm 
@@ -110,7 +116,7 @@
                                 <option value="oau" @selected($user->institution == 'oau')>OAU</option>
                                 <option value="unilag" @selected($user->institution == 'unilag')>UNILAG</option>
                                 <option value="others" @selected($user->institution == 'others')>OTHERS</option>
-                            </select>
+                            </select> --}}
                             @error('institution')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
@@ -152,14 +158,20 @@
                                 class="after:content-['*'] after:ml-0.5 after:text-red-500  text-sm font-medium text-slate-700">
                                 Category
                             </span>
-                            <select name="category" id=""
+                            <input type="text" name="category"
+                                class="h-12 mt-1 px-3 py-2 bg-white capitalize
+                        border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none
+                        focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm 
+                        focus:ring-1"
+                                disabled placeholder="Enter Category" value="{{ $user->category }}" />
+                            {{-- <select name="category" id=""
                                 class="h-12 px-3 py-2 bg-white
                             border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none
                             focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm 
                             focus:ring-1">
                                 <option value="human">Human</option>
                                 <option value="animal">Animal</option>
-                            </select>
+                            </select> --}}
                             @error('category')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
