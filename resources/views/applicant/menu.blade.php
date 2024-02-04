9a7fa1ae314ed4bcb630da4f0bf28819c33a4658
Biodun Habeeb Azeez <biodun@steer.global>
2024-02-04 23:03:38 +0100

    add icon for instruction

--- resources/views/applicant/menu.blade.php
+++ resources/views/applicant/menu.blade.php
@@ -22,11 +22,7 @@
         <a onclick="openModal()" href="#"
             class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-[#325AB3] text-white hover:text-[#34A853] border-l-4 border-transparent hover:border-indigo-500 pl-6 py-8">
             <span class="inline-flex justify-center items-center ml-4">
-                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-                </svg>
+                <img width="18" src="{{ asset('icons/instruct.png') }}" alt="">
             </span>
             <span class="ml-2 fonts-semibold text-lg tracking-wide truncate">Instruction</span>
         </a>
